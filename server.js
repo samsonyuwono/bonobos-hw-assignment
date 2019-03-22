c9f3cf840242c369b213b1f8a6298c1a2521a83b
github username <samson.yuwono@gmail.com>
2019-03-22 15:46:36 -0400

    routes are returning json as intended

--- server.js
+++ server.js
@@ -8,43 +8,113 @@ const port = 3000;
 
 app.use(morgan("combined"));
 
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "bonobos"
+});
+
 app.get("/", (req, res) => {
-  console.log("hit");
   res.send("root success");
 });
 
-app.get("/products/:id", (req, res) => {
-  console.log("fetching products:" + req.params.id);
+// app.get("/products", (req, res) => {
+//   console.log(req.params.id);
+//   const productId = req.params.id;
+//   const productQueryString = "SELECT * FROM products";
+//   connection.query(productQueryString, (err, rows, fields) => {
+//     if (err) {
+//       console.log(err);
+//       res.sendStatus(500);
+//       res.end();
+//     }
+//
+//     const products = rows.map(row => {
+//       return {
+//         "Product Id": row.product_id,
+//         "Product Name": row.product_name,
+//         "Product Image": row.product_image,
+//         "Product Description": row.product_description
+//       };
+//     });
+//     res.json(products);
+//   });
+// });
+//
+// app.get("/inventory", (req, res) => {
+//   console.log("fetching products:" + req.params.id);
+//   const inventoryQuery = "SELECT * FROM inventory";
+//   connection.query(inventoryQuery, (err, rows, fields) => {
+//     if (err) {
+//       console.log(err);
+//       res.sendStatus(500);
+//       res.end();
+//     }
+//     const inventory = rows.map(row => {
+//       return {
+//         "Product ID": row.product_id,
+//         Waist: row.waist,
+//         Length: row.length,
+//         Style: row.style,
+//         Count: row.count
+//       };
+//     });
+//     res.json(inventory);
+//   });
+// });
 
-  const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "bonobos"
-  });
+app.get("/products/inventory", (req, res) => {
+  console.log(req.params.id);
+  const joinQueryString =
+    "SELECT * FROM products JOIN inventory on products.product_id = inventory.product_id";
+  connection.query(joinQueryString, [req.params.id], (err, rows, fields) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+      res.end();
+    }
 
-  connection.query("SELECT * FROM products", (err, rows, fields) => {
-    console.log("fetching products");
-    res.json(rows);
+    const productInventories = rows.map(row => {
+      return {
+        "Product ID": row.product_id,
+        "Product Category": row.product_name,
+        "Product Image": row.product_image,
+        "Product Description": row.product_description,
+        Waist: row.waist,
+        Length: row.length,
+        Style: row.style,
+        Count: row.count
+      };
+    });
+    res.json(productInventories);
   });
-  // res.end();
 });
 
-app.get("/inventory", (req, res) => {
-  console.log("fetching products:" + req.params.id);
+app.get("/products/inventory/:id", (req, res) => {
+  const joinQueryString =
+    "SELECT * FROM products JOIN inventory on products.product_id = inventory.product_id WHERE products.product_id = ?";
+  connection.query(joinQueryString, [req.params.id], (err, rows, fields) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+      res.end();
+    }
 
-  const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "bonobos"
+    const productInventory = rows.map(row => {
+      return {
+        "Product ID": row.product_id,
+        "Product Category": row.product_name,
+        "Product Image": row.product_image,
+        "Product Description": row.product_description,
+        Waist: row.waist,
+        Length: row.length,
+        Style: row.style,
+        Count: row.count
+      };
+    });
+    res.json(productInventory);
   });
-
-  connection.query("SELECT * FROM inventory", (err, rows, fields) => {
-    console.log("fetching inventory");
-    res.json(rows);
-  });
-  // res.end();
 });
 
 app.listen(port, hostname, () => {
