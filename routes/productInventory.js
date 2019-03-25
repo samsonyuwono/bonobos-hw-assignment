bf49ea9a88d3d6420fb4e025554dcc8bc8e24290
github username <samson.yuwono@gmail.com>
2019-03-25 12:28:56 -0400

    removed product/inventory/:id route

--- routes/productInventory.js
+++ routes/productInventory.js
@@ -29,31 +29,4 @@ router.get("/products/inventory", (req, res) => {
   });
 });
 
-router.get("/products/inventory/:id", (req, res) => {
-  const productInventoryId = req.params.id;
-  const queryString =
-    "SELECT * FROM products JOIN inventory on products.product_id = inventory.product_id WHERE products.product_id = ?";
-  connection.query(queryString, [productInventoryId], (err, rows, fields) => {
-    if (err) {
-      console.log(err);
-      res.sendStatus(500);
-      res.end();
-    }
-
-    const productInventory = rows.map(row => {
-      return {
-        product_id: row.product_id,
-        product_category: row.product_name,
-        product_image: row.product_image,
-        product_description: row.product_description,
-        Waist: row.waist,
-        Length: row.length,
-        Style: row.style,
-        Count: row.count
-      };
-    });
-    res.json(productInventory);
-  });
-});
-
 module.exports = router;
